af0d6ce6c83e76cc4b85e8bdeaeb7b72343d11ef
Anthony-GG <iacano5@outlook.comgit config --global user.name Anthony-GGgit config --global user.email iacano5@outlook.com>
2023-07-17 15:58:44 -0400

    Started work on password generation, added criteriaSelection function

--- Develop/script.js
+++ Develop/script.js
@@ -10,10 +10,36 @@ function writePassword() {
 
 }
 
+//Generates password based on criteria to display on the page
+//RETURNS: STRING representing a password meeting the requested criteria
 function generatePassword(){
-  password = "THIS IS A PLACEHOLDER PASSWORD";
+  //This is the password string, currently it has a temporary placeholder.
+  var password = "THIS IS A PLACEHOLDER PASSWORD";
+
+  //declaration and initilization of password length
+  var lengthInput = 0;
+
+  //declarations and initilizations of password criteria
+  var lowercaseBool = false;
+  var uppercaseBool = false;
+  var numericBool = false;
+  var specialBool = false;
+
+  //array for the different password criteria bool
+  const criteria = [
+    lowercaseBool, uppercaseBool, numericBool, specialBool
+  ]
+
+  //for each loop prompting user to set each password crteria bool
+  criteria.forEach(criteriaSelection)
+
   return password;
 }
+
+function criteriaSelection(bool) {
+  alert("Hi");
+}
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
