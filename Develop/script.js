aaf2eeeddbd96613ecd2e08a87a81e4038d991f5
Anthony Iacano <iacano5@outlook.com>
2023-07-17 17:50:36 -0400

    removed forEach loop, replaced with standard for loop. added try catch while loop for input validation and ability to correct misinput

--- Develop/script.js
+++ Develop/script.js
@@ -25,21 +25,50 @@ function generatePassword(){
   var numericBool = false;
   var specialBool = false;
 
-  //array for the different password criteria bool
+  //array for the different password criteria bool and criteria names
   const criteria = [
-    lowercaseBool, uppercaseBool, numericBool, specialBool
+    Array("lowercase", lowercaseBool), Array("uppercase", uppercaseBool), Array("numeric", numericBool), Array("special", specialBool)
   ]
 
-  //for each loop prompting user to set each password crteria bool
-  criteria.forEach(criteriaSelection)
 
+  //for each loop prompting user to set each password crteria bool
+  for(let i=0; i < criteria.length; i++ ){
+    //while loop to prompt for answer until a valid input is entered
+    while(true){
+      //try-catch statement used for error handling on input in situations where user enters something other than 'Y' or 'N'
+      try{
+        var response = prompt("Do you want this password to have " + criteria[i][0] + " characters? (Answer with Y for yes or N for no)");
+        response = response.toUpperCase();
+        switch(response) {
+          case 'Y':
+            criteria[i][1] = true;
+            break;
+          case "N":
+            criteria[i][1] = false;
+            break;
+          //In situation where user presses cancel, allows user to leave password generation process.
+          case null:
+            var exitText = "PASSWORD GENERATION: CANCELLED"
+            return exitText;
+          default:
+            throw "Invalid input"
+        }
+        break;
+      } catch{
+        alert("Please enter a valid input.")
+      }
+  }
+    // alert(criteria[i][0])
+    // alert(criteria[i][1])
+  }
+  criteria.forEach(logToConsole);
   return password;
 }
 
-function criteriaSelection(bool) {
-  alert("Hi");
+//function for debugging
+function logToConsole(item){
+  console.log(item);
 }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
